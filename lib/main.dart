564e5f95661342aad6b6947497cd193f5ef636b0
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-07-25 00:12:34 +0900

    [이미지 검색 앱] main 오류해결 및 작성

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_search/core/router/router.dart';
+import 'package:image_search_tuesday/core/router/router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +12,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
     );
   }
 }
